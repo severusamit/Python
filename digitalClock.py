5419acf430dd9773ef60fd28ea98472e23ed0704
severusamit <87641622+severusamit@users.noreply.github.com>
2021-11-14 19:59:44 +0530

    Add files via upload

---
+++ digitalClock.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title('Digital clock by Severus')
+
+def clock():
+	tick = strftime('%H:%M:%S %p')
+	label.config(text =tick)
+	label.after(1000, clock)
+
+label = Label(root, font =('sans', 80), background = 'black', foreground = 'white')
+label.pack(anchor= 'center')
+
+clock()
+mainloop()
